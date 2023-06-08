16f8b66872d7039dba516af3579f5d7d0bc9012d
tornikegogilashvili <tornikegogilashvili@yahoo.com>
2023-06-08 18:38:50 +0400

    input changes

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { FormContainer } from "../atoms";
 import {Input} from "../atoms/Input"
+import { UseForm } from "../../hooks/UseForm";
+import { generateRegisterFormValues } from "./generateRegisterFormValues";
 
 
 export const RegisterForm = () => {
+    const {formValues: registerFormValues, onFormChange} = UseForm(generateRegisterFormValues());
+    console.log("formValues", registerFormValues);
     return(
         <FormContainer>
-            <Input name="firstName" label="firstName" />
-            <Input name="lastName" label="lastName" />
-            <Input name="email" label="email" />
-            <Input name="password" label="password" />
+            <Input name="firstName" label="firstName" value={registerFormValues.firstName.value}
+            error={registerFormValues.firstName.error}
+            onChange={onFormChange} />
+            <Input name="lastName" label="lastName" value={registerFormValues.lastName.value}
+            error={registerFormValues.lastName.error}
+            onChange={onFormChange} />
+            <Input name="email" label="email" value={registerFormValues.email.value} 
+            error={registerFormValues.email.error}
+            onChange={onFormChange} />
+            <Input name="password" label="password" value={registerFormValues.password.value}
+            error={registerFormValues.password.error}
+            onChange={onFormChange} />
         </FormContainer>
     )
 }
--- src/hooks/UseForm.js
+++ src/hooks/UseForm.js
@@ -1,3 +1,30 @@
-export const UseForm = () => {
-    return {};
+import { useState } from "react";
+
+export const UseForm = (defaultFormValues) => {
+    const [formValues, setFormValues] = useState(defaultFormValues);
+
+    const onFormChange = (event) => {
+        const {name , value} = event.target;
+        const {validateInput} = formValues[name]
+        setFormValues((prevFormValues) => {
+            return {
+                ...prevFormValues,
+                [name] : {
+                    ...prevFormValues[name],
+                    value,
+                    error:validateInput ? validateInput(value):null,
+                },
+            };
+        });
+    };
+
+    const clearForm = (data) => {
+        setFormValues(data);
+    }
+
+    return {
+        formValues,
+        onFormChange,
+        clearForm,
+    };
 }
