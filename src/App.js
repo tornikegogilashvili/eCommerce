d4ae13b71ea68fd6643333d0e1ac834e62f7306f
tornikegogilashvili <tornikegogilashvili@yahoo.com>
2023-06-15 22:16:44 +0400

    send product request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Link} from "react-router-dom"
 import { RouteComponent } from "./Routes";
 import './App.css';
 
 const  App = () => {
   return(
     <div>
+      <Link to="/product/new" >add Product</Link>
       <RouteComponent />
     </div>
   )
--- src/helper/instance.js
+++ src/helper/instance.js
@@ -8,5 +8,29 @@ export const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use((req) =>{
     const token = localStorage.getItem("token");
-    req.headers.Authorization = `bearer ${token}`;
+    req.headers.Authorization = `Bearer ${token}`;
+    return req;
 });
+
+
+axiosInstance.interceptors.response.use(
+    (res) => {
+        return res;
+    },
+    (response) => {
+        console.log("error", response);
+        const originalRequest = response.config;
+        if(
+            response.response.status === 401 && 
+            response?.response?.data?.message === "token not valid" ) {
+                const refreshToken = localStorage.getItem("refreshToken");
+                axios.post("http://localhost:3001/users/refresh", {
+                    refresh_token: refreshToken,
+                })
+                .then(({ data }) => {
+                    localStorage.setItem("token", data.token);
+                    return axiosInstance(originalRequest)
+                });
+            }
+    }
+);
